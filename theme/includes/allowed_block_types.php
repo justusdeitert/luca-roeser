7f494328b6936986f3e88cc94f368515fee9d19f
Justus Deitert <mail@justusdeitert.de>
2021-06-13 08:36:19 +0200

    Add allowed block types functionality

---
+++ theme/includes/allowed_block_types.php
@@ -0,0 +1,95 @@
+<?php
+
+$core_blocks = [
+    // Text
+    'core/paragraph',
+    'core/heading',
+    'core/list',
+    'core/quote',
+    'core/code',
+    // 'core/freeform',
+    'core/preformatted',
+    // 'core/pullquote',
+    'core/table',
+    // 'core/verse',
+
+    // Media
+    // 'core/image',
+    // 'core/gallery',
+    // 'core/audio',
+    // 'core/cover',
+    // 'core/file',
+    // 'core/media-text',
+    // 'core/video',
+
+    // Design
+    // 'core/buttons',
+    'core/columns',
+    'core/group',
+    // 'core/more',
+    // 'core/nextpage',
+    // 'core/separator',
+    'core/spacer',
+    // 'core/site-logo',
+    // 'core/site-tagline',
+    // 'core/site-title',
+    // 'core/query-title',
+    // 'core/post-terms',
+
+    // Widgets
+    'core/shortcode',
+    // 'core/archives',
+    // 'core/calendar',
+    // 'core/categories',
+    'core/html',
+    // 'core/latest-comments',
+    // 'core/latest-posts',
+    // 'core/page-list',
+    // 'core/rss',
+    // 'core/social-links',
+    // 'core/tag-cloud',
+    'core/search',
+
+    // Theme
+    // 'core/query',
+    // 'core/post-title',
+    // 'core/post-content',
+    // 'core/post-date',
+    // 'core/post-excerpt',
+    // 'core/post-featured-image',
+    // 'core/loginout',
+];
+
+$custom_blocks_normal_editor = [
+    'custom/divider',
+    'custom/accordion',
+    'custom/map',
+    'custom/icon',
+    'custom/image-header',
+    'custom/text-image',
+    'custom/slider',
+];
+
+$custom_blocks_widget_editor = [
+    'custom/divider',
+    'custom/icon',
+    'custom/text-image',
+    'custom/slider',
+];
+
+add_filter('allowed_block_types_all', function ($allowed_block_types, $editor_context) {
+
+    global $core_blocks, $custom_blocks_normal_editor, $custom_blocks_widget_editor, $pagenow;
+
+    /**
+     * Widget Editor
+     */
+    if ($pagenow === 'widgets.php') {
+        return array_merge($core_blocks, $custom_blocks_widget_editor);
+    }
+
+    /**
+     * Normal Editor Pages
+     */
+    return array_merge($core_blocks, $custom_blocks_normal_editor);
+}, 10, 2);
