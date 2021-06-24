3a5f095234877199080d1ee5d998fc1d07151939
Justus Deitert <mail@justusdeitert.de>
2021-06-24 15:02:24 +0200

    Fixes index.php

--- theme/index.php
+++ theme/index.php
@@ -4,8 +4,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <?php wp_head(); ?>
-
-
 </head>
 
     <body <?php body_class(); ?>>
@@ -18,4 +16,5 @@
 
         <?php do_action('get_footer'); ?>
         <?php wp_footer(); ?>
+    </body>
 </html>
