00f80472dfa374b520405fc39b4b71b3a77713fd
Justus Deitert <mail@justusdeitert.de>
2021-06-26 15:17:43 +0200

    Removes gutenberg block styles

---
+++ theme/includes/remove-block-styles.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Remove Gutenberg Block Library CSS from loading on the frontend
+ */
+
+add_action('wp_enqueue_scripts', function() {
+    wp_dequeue_style('wp-block-library');
+    wp_dequeue_style('wp-block-library-theme');
+    // wp_dequeue_style('wc-block-style'); // Remove WooCommerce block CSS
+}, 100);
