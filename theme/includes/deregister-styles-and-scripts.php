a9932601f52c5630e883510087f49ff49a86bac9
Justus Deitert <mail@justusdeitert.de>
2021-07-03 16:16:28 +0200

    Contact form 7

--- theme/includes/deregister-styles-and-scripts.php
+++ theme/includes/deregister-styles-and-scripts.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Remove Scripts
+ * Deregister jQuery
  */
 add_action('init', function () {
     if (!is_admin() && !is_login_page()) {
@@ -10,13 +10,15 @@ add_action('init', function () {
 });
 
 /**
- * Remove Styles
+ * Contact form 7
  */
 add_action( 'wp_enqueue_scripts', function() {
     if (!is_admin() && !is_login_page()) {
 
-        // TODO: Make Contact Form 7 Only Load When Module is Present!
-        wp_dequeue_script('contact-form-7');
         wp_dequeue_style('contact-form-7');
+
+        if (!has_shortcode(get_post()->post_content, 'contact-form-7')) {
+            wp_dequeue_script('contact-form-7');
+        }
     }
 });
