bb4b4927c7f5248bbcae0e0682eda68c329bb4e0
Justus Deitert <mail@justusdeitert.de>
2021-07-10 13:44:27 +0200

    Adjust lightgallery and adds zoom plugin

--- theme/resources/scripts/external/lightgallery.js
+++ theme/resources/scripts/external/lightgallery.js
@@ -7,14 +7,14 @@ import lightGallery from 'lightgallery';
  * Plugins
  */
 import lgThumbnail from 'lightgallery/plugins/thumbnail'
-// import lgZoom from 'lightgallery/plugins/zoom'
+import lgZoom from 'lightgallery/plugins/zoom'
 import lgHash from 'lightgallery/plugins/hash'
 
 window.initTextImageLightGallery = () => {
     document.querySelectorAll('.text-image-block').forEach(textImageBlock => {
         let imageWrapper = textImageBlock.querySelector('.text-image-block__image-wrapper');
         let lightGalleryInstance = lightGallery(imageWrapper, {
-            plugins: [lgThumbnail, lgHash],
+            plugins: [lgZoom, lgThumbnail, lgHash],
             speed: 300,
             thumbnail: true,
             customSlideName: true,
