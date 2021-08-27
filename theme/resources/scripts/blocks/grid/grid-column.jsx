8f7163600a3a32d689ecbb1bfe9be28736e5cc04
Justus Deitert <mail@justusdeitert.de>
2021-08-27 10:47:29 +0200

    Remove stray parenthesis

--- theme/resources/scripts/blocks/grid/grid-column.jsx
+++ theme/resources/scripts/blocks/grid/grid-column.jsx
@@ -228,7 +228,7 @@ registerBlockType('custom/grid-column', {
                                             setAttributes({columnSizeDesktop: value})
                                         }}
                                     />
-                                </ResetWrapperControl>(
+                                </ResetWrapperControl>
                             </MobileSwitchInner>
                             <MobileSwitchInner type={'tablet'}>
                                 <ResetWrapperControl onClick={() => {
