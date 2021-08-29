6155707131d1d68f3e0fbebb7488fdee7a7b7a7f
Justus Deitert <mail@justusdeitert.de>
2021-08-29 14:20:26 +0200

    Add video functionality

--- theme/resources/scripts/editor-scripts/videos.js
+++ theme/resources/scripts/editor-scripts/videos.js
@@ -1,27 +1,44 @@
 const videoElements = document.querySelectorAll('.video-block__video');
+let userInteracted = false;
+
+window.addEventListener('click', () => {
+    userInteracted = true;
+
+    // Unmute all videos once user interacted
+    // TODO: Implement filter for videoblock
+    videoElements.forEach(videoElement => {
+        videoElement.muted = false;
+    })
+});
 
 videoElements.forEach(videoElement => {
 
     let videoIsClicked = false;
 
-    videoElement.addEventListener('click', () => {
+    videoElement.addEventListener('click', (event) => {
+        if (videoElement.muted) {
+            event.preventDefault();
+        }
+
         videoIsClicked = true;
     });
 
     if (videoElement.classList.contains('play-in-view')) {
         window.ScrollTrigger.create({
             trigger: videoElement,
-            start: 'top 70%',
-            end: 'bottom 30%',
+            start: 'top 65%',
+            end: 'bottom 35%',
             // markers: true,
             onEnter: () => {
                 if (!videoIsClicked && videoElement.paused) {
                     videoElement.play();
+                    userInteracted && (videoElement.muted = false);
                 }
             },
             onEnterBack: () => {
                 if (!videoIsClicked && videoElement.paused) {
                     videoElement.play();
+                    userInteracted && (videoElement.muted = false);
                 }
             },
             onLeave: () => {
